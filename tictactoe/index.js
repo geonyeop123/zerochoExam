63a05b0004445aabe2301da12d85711dfdf4eaa0
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2020-10-13 22:39:15 +0900

    10.13
    tbody를 쓰는 이유를 알았도다

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,17 +1,43 @@
 "use strict";
 const tableTag = document.querySelector("table");
-const tbodyTag = document.querySelector("tbody");
-let i, j, temp;
-
-for (i = 0; i < 3; i++) {
-  const tr = document.createElement("tr");
-  tbodyTag.appendChild("tr");
-  console.log("hi");
-  for (j = 0; j < 3; j++) {
-    const td = document.createElement("td");
-    td.addEventListener("click", (e) => {
-      console.log(e.target);
-    });
-    tr.appendChild("td");
-  }
-}
+const bodyTag = document.querySelector("body");
+const startBtn = document.querySelector('#startBtn');
+let i, j, count = 0;
+let dataset = [];
+startBtn.addEventListener('click',()=>{
+  bodyTag.innerHTML = '';
+  dataset = [];
+  for (i = 0; i < 3; i++) {
+    const columnTag = document.createElement("column");
+    bodyTag.appendChild(columnTag);
+    const data = [];
+    for (j = 0; j < 3; j++) {
+      const divTag = document.createElement("div");
+      divTag.addEventListener("click", (e) => {
+        const body = e.target.parentNode.parentNode;
+        const column = e.target.parentNode;
+        const width = Array.prototype.indexOf.call(body.children, column) - 1;
+        const height = Array.prototype.indexOf.call(column.children, divTag);
+        if(dataset[width][height] === 1){
+          return
+        }
+        e.target.innerText = 'O';
+        dataset[width][height] = 1;
+        count++;
+        if(count === 9){
+          setTimeout(()=>{
+            const yesOrNo = confirm('게임이 종료되었습니다. 다시 하시겠습니까?');
+            if(yesOrNo){
+              console.log('hi');
+            }else{
+              console.log("bye");
+            }
+          },100)
+        }
+      });
+      columnTag.appendChild(divTag);
+      data.push(0);
+    }
+    dataset.push(data);
+  } 
+})
